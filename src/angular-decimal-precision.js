c95584918b3c50aadfe05dff46659c5e8f7ee429
jasonp <jasonp@isell.com.au>
2019-07-23 15:22:49 +0800

    updated structure

--- src/angular-decimal-precision.js
+++ src/angular-decimal-precision.js
@@ -1,34 +1,17 @@
 /**
  * Angularjs Wrapper for Decimal.js
  * Author: Jason P <jmpanugaling@gmail.com> 2019
- * 
+ *
  */
-(function (app) {
-	'use strict';
+(function() {
+  "use strict";
 
-	if (typeof exports === 'object') {
-		module.exports = app(
-			typeof angular !== 'undefined' ? angular : require('angular'),
-			typeof Decimal !== 'undefined' ? Decimal : require('decimal.js')
-		);
-	} else if (typeof define === 'function' && define.amd) {
-		define(['angular', 'decimal'], app);
-	} else {
-		if (typeof angular === 'undefined') {
-			throw new Error('Angular.Js not found');
-		}
+  /* @ngInject */
+  function PrecisionFactory() {
+    return new Decimal();
+  }
 
-		app(angular, Decimal);
-	}
-}(function (angular, Decimal) {
-	'use strict';
-
-	/* @ngInject */
-	function PrecisionFactory() {
-		return new Decimal();
-	}
-	
-	return angular
-		.module('jmp.decimal-precision', [])
-		.factory('DecimalPrecisionFactory', [PrecisionFactory]);
-}));
+  angular
+    .module("jmp.decimal-precision", [])
+    .factory("DecimalPrecisionFactory", [PrecisionFactory]);
+})();
